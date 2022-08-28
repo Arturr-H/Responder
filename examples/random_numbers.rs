0383e8ed90f8da03cfe5c84aa9837013c4cff45a
Artur <artur.h.0427@gmail.com>
2022-08-28 07:55:21 +0200

    Add examples

---
+++ examples/random_numbers.rs
@@ -0,0 +1,42 @@
+// Go to localhost:8080 to see the result
+
+/*- Imports -*/
+use std::net::TcpStream;
+use rust_web_framework::{*, response::Respond, response::respond};
+use rand::Rng;
+
+/*- Initialize -*/
+fn main() {
+
+    /*- Initiaize routes -*/
+    let routes = Route::Stack("", &[
+        Route::Tail(Method::GET, "", Function::S(respond_with_random_number)),
+    ]);
+
+    /*- Initiaize server -*/
+    start(ServerConfig {
+        addr: "127.0.0.1",
+        port: 8080u16,
+        serve: None,
+        not_found: None,
+        routes,
+    }).unwrap();
+}
+
+/*- Api endpoints -*/
+fn respond_with_random_number(mut stream:&mut TcpStream) -> () {
+    /*- Get random number -*/
+    let random_number = rand::thread_rng().gen_range(0..100);
+
+    /*- Respond with the random number -*/
+    respond(
+        &mut stream,
+        200u16,
+        Respond::text(
+            &format!(
+                "Random number between 0 and 100: {}",
+                &random_number
+            )
+        )
+    );
+}
---
+++ examples/serve_html.rs
@@ -0,0 +1,33 @@
+// Go to localhost:8080/index.html or localhost:8080/manual_serve to see the result
+
+/*- Imports -*/
+use std::net::TcpStream;
+use rust_web_framework::{*, response::with_file, response::respond};
+
+/*- Initialize -*/
+fn main() {
+
+    /*- Initiaize routes -*/
+    let routes = Route::Stack("", &[
+        Route::Tail(Method::GET, "manual_serve", Function::S(manual_serve)),
+    ]);
+
+    /*- Initiaize server -*/
+    start(ServerConfig {
+        addr: "127.0.0.1",
+        port: 8080u16,
+        serve: Some("./examples/static"),
+        not_found: None,
+        routes,
+    }).unwrap();
+}
+
+/*- Api endpoints -*/
+fn manual_serve(mut stream:&mut TcpStream) -> () {
+    /*- Respond with the html file -*/
+    respond(
+        &mut stream,
+        200u16,
+        with_file("examples/static/manual.html")
+    );
+}
